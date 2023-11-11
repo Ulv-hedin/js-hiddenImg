c262cff23aef7725e83a5d5eecb406c14e833fce
Ulv <ulv@o2.pl>
2023-11-11 23:42:25 +0100

    test

--- js/main.js
+++ js/main.js
@@ -5,12 +5,10 @@ const revArr = document.querySelector('.fas')
 const addClass = () => {
 	hidImg.classList.toggle('hide')
 
-
 	if (hidImg.classList.contains('hide')) {
 		arrow.style.rotate = '180deg'
 	} else {
 		arrow.style.rotate = '0deg'
 	}
-
 }
 arrow.addEventListener('click', addClass)
