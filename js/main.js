8aa2973605c2a57e922ad0b35efa36f44dc7d397
Ulv <ulv@o2.pl>
2023-11-11 23:38:46 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const arrow = document.querySelector('.arrow')
+const hidImg = document.querySelector('.item1')
+const revArr = document.querySelector('.fas')
+
+const addClass = () => {
+	hidImg.classList.toggle('hide')
+
+
+	if (hidImg.classList.contains('hide')) {
+		arrow.style.rotate = '180deg'
+	} else {
+		arrow.style.rotate = '0deg'
+	}
+
+}
+arrow.addEventListener('click', addClass)
